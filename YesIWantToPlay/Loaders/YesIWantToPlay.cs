ba32f0bd663df1f6617c881cb4dca1ecf7daaaf3
TacoGuyAT <apotroend@outlook.com>
2023-06-24 02:13:21 +0300

    Add project files.

---
+++ YesIWantToPlay/Loaders/YesIWantToPlay.cs
@@ -0,0 +1,11 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(YesIWantToPlay.Loaders.YIWTPMelon), "YesIWantToPlay", 0, 2, 0, "__tacoguy", "https://github.com/TacoGuyAT/yiwtp-sr2")]
+[assembly: MelonColor(255, 255, 255, 0)]
+
+namespace YesIWantToPlay.Loaders;
+public class YIWTPMelon : MelonMod {
+    public override void OnInitializeMelon() {
+        LoggerInstance.Msg("patched, glhf");
+    }
+}
